6f2a6d2d64259269445ecaaf077d37ede738c780
Michael-Lee-1994 <michael.lee.1994.flatiron@gmail.com>
2020-11-16 15:13:56 -0800

    commit tree 0246407262de13673cafa049b3b6e7830d4bffa7

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code here
+var element = document.querySelector("main#main").remove();
+
+var newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Flatiron School is the champion!";
